932e43689d9dae21f1ab90a8cb77b8df98445aee
joshua-eggleston <jeggleston1980@gmail.com>
2024-06-23 01:28:26 -0500

    tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,18 +1,42 @@
 import {View, Text, Image, StyleSheet} from 'react-native';
 import { TweetType } from '@/types';
+import { Entypo } from '@expo/vector-icons';
+import IconButton from './IconButton';
+
 
 type TweetProps = {
   tweet: TweetType;
 }
 const Tweet = ({tweet}: TweetProps) => {
-
     return (
     <View style={styles.container}>
       <Image src={tweet.user.image} style={styles.userImage} />
 
       <View style={styles.mainContainer}>
+        <View style={{flexDirection: 'row'}}>
         <Text style={styles.name}>{tweet.user.name}</Text>
+      <Text style={styles.username}>{tweet.user.username} · 2hr </Text>
+      <Entypo 
+      name="dots-three-horizontal" 
+      size={16} 
+      color="grey" 
+      style= {{marginLeft: 'auto'}}
+      />
+        
+        </View>
         <Text style={styles.content}>{tweet.content}</Text>
+
+        {tweet.image && <Image src={tweet.image} style={styles.image}/>}
+
+        <View style={styles.footer}>
+        <IconButton icon='comment' text={tweet.numberOfComments}/>
+        <IconButton icon='retweet' text={tweet.numberOfRetweets}/>
+        <IconButton icon='heart' text={tweet.numberOfLikes}/>
+        <IconButton icon='chart' text={tweet.impressions || 0}/>
+        <IconButton icon='share-apple' />
+
+
+        </View>
       </View>
     </View>
     )
@@ -38,10 +62,26 @@ const styles = StyleSheet.create({
     name: {
       fontWeight: '600',
     },
+    username:{
+      color: 'grey',
+      marginLeft: 5,
+    },
     content: {
       lineHeight: 20,
       marginTop: 5,
     },
+    image: {
+      width: '100%',
+      aspectRatio: 19 / 9,
+      marginVertical: 10,
+      borderRadius: 15,
+    },
+    //footer
+    footer: {
+      flexDirection: 'row',
+      marginVertical: 5,
+      justifyContent: 'space-around'
+    }
   });
 
   export default Tweet;
